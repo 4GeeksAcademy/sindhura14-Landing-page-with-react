5083f348204b894e2607a3a62b061258f22dbee3
sindhura14 <sindhu.kilaru14@gmail.com>
2024-05-16 00:47:57 +0000

    css changes to set the navbar

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import { useState } from "react";
 
 
-const navItems = [{name:'Home', color:'white',active:true ,stylename:"nav-link"},
-{name:'About', color:'grey',active:false ,stylename:"nav-link"},
-{name:'Services', color:'grey',active:false ,stylename:"nav-link"},
-{name:'Contact', color:'grey',active:false ,stylename:"nav-link"}
+const navItems = [{name:'Home', color:'white',active:true ,class:"nav-link"},
+{name:'About', color:'grey',active:false ,class:"nav-link"},
+{name:'Services', color:'grey',active:false ,class:"nav-link"},
+{name:'Contact', color:'grey',active:false ,class:"nav-link"}
 ];
 
 
 function NavBarItems(){
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" >
-    <a className="navbar-brand ms-2" href="#" style={{color:"white"}}>Start Bootstrap</a>
     <div className="container-fluid">
-   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <a className="navbar-brand ms-1" href="#" style={{color:"white"}}>Start Bootstrap</a>
+   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
      <span className="navbar-toggler-icon"></span>
    </button>
-   <div className="collapse navbar-collapse d-flex " id="navbarText">
-     <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end">
+   <div className="collapse navbar-collapse" id="navbarNav">
+     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
       {navItems.map(function(item,i){
        return <NavItem key={i} data={item}/>
       })}
@@ -31,7 +31,7 @@ function NavBarItems(){
 
 
 function NavItem({data}){
-   return(<li className="nav-item"><a className={data.stylename} href="#" style={{color:data.color}}>{data.name}</a></li>)
+   return(<li className="nav-item"><a className={`${data.class} ${data.active ? 'active' : ''}`} href="#" style={{color:data.color}}>{data.name}</a></li>)
  }
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,47 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+//import navigaton component
+import NavBarItems from "./component/navbar.jsx";
+
+import MainContent from "./component/main.jsx";
+
+import Cards from "./component/cards.jsx";
+
+const cardItems = [
+    {
+        title:"Card Title",
+        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        buttonLabel: "Find Out More"
+    },
+    {
+        title:"Card Title",
+        text:"dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.",
+        buttonLabel: "Find Out More"
+    },
+    {
+        title:"Card Title",
+        text:"we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will.",
+        buttonLabel: "Find Out More"
+    },
+    {
+        title:"Card Title",
+        text:"Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus",
+        buttonLabel:"Find Out More"
+
+    }
+];
+
+function App(){
+    return(
+    <div>
+        <NavBarItems></NavBarItems>
+        <MainContent></MainContent>
+        {cardItems.map(function(item,i){
+            return <Cards key={i} data={item}></Cards>
+        })}
+     </div>)
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<App/>, document.querySelector("#app"));
